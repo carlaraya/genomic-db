c3478728419d8ff0b7a617ad981c4f83e264a5f6
carlaraya <carl.araya256@gmail.com>
2020-02-22 16:46:50 +0800

    Implement validation

--- web-app/app/models/dataset.rb
+++ web-app/app/models/dataset.rb
@@ -1,2 +1,6 @@
 class Dataset < ApplicationRecord
+  validates :name, length: { in: 10..100 }
+  validates :description, length: { maximum: 1000 }
+  validates :uploader, length: { in: 3..100 }
+  validates :institute, length: { in: 3..100 }
 end
